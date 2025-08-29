b6fc2a5e58391bc53010b0521d1554e7d3d006b7
dimovdmitrii <dimovdima1509@gmail.com>
2025-08-29 11:31:33 +0200

    initialisation commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const mainTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#6750A4",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#6750A40D",
+    },
+  },
+  typography: {
+    fontFamily: "Inter",
+    fontStyle: "normal",
+    h1: {
+      fontWeight: 500,
+      fontSize: 20,
+    },
+    h2: {
+      fontWeight: 500,
+      fontSize: 14,
+      letterSpacing: 0.1,
+      lineHeight: 20,
+    },
+    body1: {
+      fontSize: 16,
+      fontWeight: 400,
+    },
+    body2: {
+      fontSize: 14,
+      fontWeight: 400,
+    },
+  },
+  spacing: 8,
+});
+
+function App() {
+  return <ThemeProvider theme={mainTheme}></ThemeProvider>;
+}
+
+export default App;
